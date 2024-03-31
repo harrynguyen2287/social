ab54de5c74465569a00f6e5edac7f0407fd28a07
harrynguyen2287 <harrynguyen2287@gmail.com>
2024-03-31 22:08:30 +1100

    Init

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ut nulla possimus fuga, eligendi, exercitationem eum amet officia culpa repellat mollitia. Fugiat corporis quia deserunt cumque accusantium ex, excepturi commodi.</p>
+          <span>Don't you have an account</span>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,7 @@
+import "./register.scss";
+
+const Register = () => {
+  return <div className="login">Register</div>;
+};
+
+export default Register;
